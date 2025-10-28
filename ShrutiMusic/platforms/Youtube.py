7259142733dd695f5350c575906aa00e0be2ff10
Nahi Malum <ponygc56@gmail.com>
2025-10-28 07:19:39 +0530

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/platforms/Youtube.py
+++ ShrutiMusic/platforms/Youtube.py
@@ -8,8 +8,8 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
-from ShrutixMusic.utils.database import is_on_off
-from ShrutixMusic.utils.formatters import time_to_seconds
+from ShrutiMusic.utils.database import is_on_off
+from ShrutiMusic.utils.formatters import time_to_seconds
 import os
 import glob
 import random
